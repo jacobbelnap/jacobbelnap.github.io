a9a51d47eec641a110e632a8ea4e21bb109e003c
jacobbelnap <bel15040@byui.edu>
2020-04-24 12:07:48 -0600

    update

--- scripts/curDate.js
+++ scripts/curDate.js
@@ -1,5 +1,6 @@
 var curDate = new Date();
 var lastModified = new Date(document.lastModified);
+lastModified = Date.parse(lastModified);
 var curYear = curDate.getFullYear();
 document.getElementById("curYear").innerHTML = curYear;
 document.getElementById("lastModified").innerHTML = lastModified;
