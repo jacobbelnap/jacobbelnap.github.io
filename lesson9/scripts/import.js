8263cee2b5b6828950be62384c73b7eceba6283d
bel15040@byui.edu <bel15040@byui.edu>
2020-11-18 15:54:33 -0700

    d

--- lesson9/scripts/import.js
+++ lesson9/scripts/import.js
@@ -11,31 +11,44 @@ window.addEventListener('load', (event)=>{
          console.table(jsonObject);
          const towns = jsonObject['towns'];
          for (let i = 0; i < towns.length; i++) {
+         
                let card = document.createElement('section');
                let name = document.createElement('h2');
                let motto = document.createElement('p');
+               let yearFounded = document.createElement('p');
                let currentPopulation = document.createElement('p');
-               let photo = document.createElement('img')
+               let averageRainfall = document.createElement('p');
+               let photo = document.createElement('img');
 
+               if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
+                  //Append Name
+                  name.textContent = towns[i].name + ', ID';
+                  card.appendChild(name);
 
-               //Append Name
-               name.textContent = towns[i].name + ', ID';
-               card.appendChild(name);
+                  //Append Motto
+                  motto.textContent = 'Motto: ' + towns[i].motto;
+                  card.appendChild(motto);
 
-               //Append Motto
-               motto.textContent = 'Motto: ' + towns[i].motto;
-               card.appendChild(motto);
+                  //Append Year Founded
+                  yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                  card.appendChild(yearFounded);
 
-               //Append Population
-               currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
-               card.appendChild(currentPopulation);
+                  //Append Population
+                  currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
+                  card.appendChild(currentPopulation);
 
-               //Append Image
-               photo.setAttribute('src', '/images/' + towns[i].photo);
-               card.appendChild(photo)
+                  //Append Rain Fall
+                  averageRainfall.textContent = 'Annual Average Rainfall: ' + towns[i].averageRainfall + ' in';
+                  card.appendChild(averageRainfall);
 
-               //Add entire card to HTML
-               document.querySelector('div.towns').appendChild(card);
+                  //Append Image
+                  photo.setAttribute('src', '/images/' + towns[i].photo);
+                  card.appendChild(photo)
+
+                  //Add entire card to HTML
+                  document.querySelector('div.towns').appendChild(card);
+               }
+           
       }
-   });
+      });
 });
