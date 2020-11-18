4c4f147939fd647227eae2540b36a726996dc52d
bel15040@byui.edu <bel15040@byui.edu>
2020-11-18 11:11:51 -0700

    up

--- lesson9/scripts/import.js
+++ lesson9/scripts/import.js
@@ -1,38 +1,41 @@
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+window.addEventListener('load', (event)=>{
+
+   const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 
-fetch(requestURL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(jsonObject) {
-        console.table(jsonObject);
-        const towns = jsonObject['towns'];
-        for (let i = 0; i < towns.length; i++) {
-            let card = document.createElement('section');
-            let name = document.createElement('h2');
-            let motto = document.createElement('p');
-            let currentPopulation = document.createElement('p');
-            let photo = document.createElement('img')
+   fetch(requestURL)
+      .then(function(response) {
+         return response.json();
+      })
+      .then(function(jsonObject) {
+         console.table(jsonObject);
+         const towns = jsonObject['towns'];
+         for (let i = 0; i < towns.length; i++) {
+               let card = document.createElement('section');
+               let name = document.createElement('h2');
+               let motto = document.createElement('p');
+               let currentPopulation = document.createElement('p');
+               let photo = document.createElement('img')
 
 
-            //Append Name
-            name.textContent = towns[i].name + ', ID';
-            card.appendChild(name);
+               //Append Name
+               name.textContent = towns[i].name + ', ID';
+               card.appendChild(name);
 
-            //Append Motto
-            motto.textContent = 'Motto: ' + towns[i].motto;
-            card.appendChild(motto);
+               //Append Motto
+               motto.textContent = 'Motto: ' + towns[i].motto;
+               card.appendChild(motto);
 
-            //Append Population
-            currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
-            card.appendChild(currentPopulation);
+               //Append Population
+               currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
+               card.appendChild(currentPopulation);
 
-            //Append Image
-            photo.setAttribute('src', '/images/' + towns[i].photo);
-            card.appendChild(photo)
+               //Append Image
+               photo.setAttribute('src', '/images/' + towns[i].photo);
+               card.appendChild(photo)
 
-            //Add entire card to HTML
-            document.querySelector('div.towns').appendChild(card);
-        }
-    });
+               //Add entire card to HTML
+               document.querySelector('div.towns').appendChild(card);
+      }
+   });
+});
