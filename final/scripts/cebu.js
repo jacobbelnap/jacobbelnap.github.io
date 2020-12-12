7f15531dae80345097e31e78a898bec34db9d2f1
bel15040@byui.edu <bel15040@byui.edu>
2020-12-12 13:45:37 -0700

    h

--- final/scripts/cebu.js
+++ final/scripts/cebu.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', (event)=>{
    // Sources
    const requestURL = "https://jacobbelnap.github.io/final/scripts/temples.json";
-   // Temples JSON
+   const featuredCity= "Cebu";
    fetch(requestURL)
    .then(function (response) {
        return response.json();
@@ -12,51 +12,110 @@ window.addEventListener('load', (event)=>{
        for (i = 0; i < temples.length; i++) {
             
         let card = document.createElement('section');
-            let title = document.createElement('h3');
-            let name = document.createElement('h1');
-            let phone = document.createElement('p');
-            let address = document.createElement('p');
-            let dedicated = document.createElement('p');
-            let services = document.createElement('p');
+        let left = document.createElement('section');
+        let right = document.createElement('section');
+        let title = document.createElement('h3');
+        let name = document.createElement('h1');
+        let phone = document.createElement('p');
+        let address = document.createElement('p');
+        let phase = document.createElement('p');
+        let dedicated = document.createElement('p');
+        let services = document.createElement('p');
+        let photo = document.createElement('img');
 
-           // Find 18:00
-            if (temples[i].name == "Cebu"){  
-            
-                // Title
-                title.textContent = 'Featured City'
-                card.appendChild(title);
+        let fCard = document.createElement('section');
+        let fPhoto = document.createElement('img');
+        let fLink = document.createElement('a');
+        let fDesc = document.createElement('h4');
 
-                // Name
-                name.textContent = temples[i].name;
-                card.appendChild(name);
-
-                // Phone
-                phone.textContent = temples[i].telephone;
-                card.appendChild(phone);
-
-                // Address
-                address.textContent =temples[i].address;
-                card.appendChild(address);
-
-                // Dedicated
-                dedicated.textContent= temples[i].dedicated;
-                card.appendChild(dedicated);
-
-                // Services
-                services.textContent = temples[i].services;
-                card.appendChild(services);
-
-                // Photo
-                var photo = temples[i].image;
-                let imagesrc = 'images/' + photo; 
-                document.getElementById(fPhoto).setAttribute('src', imagesrc);
-                document.getElementById(fPhoto).setAttribute('alt', photo);
-                
-                //Add entire card to HTML
-                document.querySelector('div.featured').appendChild(card);
+        // Choose Featured
+        if (temples[i].name == featuredCity){  
         
+            // Title
+            title.textContent = 'Featured City'
+            document.querySelector('div.featured').appendChild(title);
+
+            // Name
+            name.textContent = '"'+temples[i].name+'"';
+            document.querySelector('div.featured').appendChild(name);
+
+            // Phone
+            phone.textContent = 'Phone: '+temples[i].telephone;
+            left.appendChild(phone);
+
+            // Address
+            address.textContent ='Address: '+temples[i].address;
+            left.appendChild(address);
+
+            // Phase
+            phase.textContent ='Phase: '+temples[i].phase;
+            left.appendChild(phase);
+
+            // Dedicated
+            dedicated.textContent= 'Dedicated: '+temples[i].dedicated;
+            left.appendChild(dedicated);
+
+            // Services
+            var service = temples[i].services[0];
+            for (j=1;j<temples[i].services.length;j++){
+                service = service + ', ' + temples[i].services[j];
             }
-       }
-   });
+            services.textContent = 'Services: '+service;
+            left.appendChild(services);
+
+            // Photo
+            photo.setAttribute('src', 'images/'+ temples[i].image)
+            right.appendChild(photo);
+      
+            //Add entire card to HTML
+            card.appendChild(left);
+            card.appendChild(right);
+            card.setAttribute('id','fCard');
+            document.querySelector('div.featured').appendChild(card);
+
+
+
+
+            // -------------------------------Featured Card-------------------------------
+            // Photo Description
+            fDesc.textContent = temples[i].name; 
+
+            // Photo
+            fPhoto.setAttribute('src','images/' + fDesc.textContent + 'Hotel.jpg');
+            fPhoto.setAttribute('alt', fDesc.textContent + ' Hotel');
+
+            fCard.appendChild(fPhoto);
+            fCard.appendChild(fDesc);  
+
+            // A tag
+            fLink.appendChild(fCard);
+            fLink.setAttribute('href', temples[i].name + '.html');
+            
+            // Append to Doc
+            document.querySelector('div.pic').appendChild(fLink);
+        }
+        else{
+            //City Name
+            fDesc.textContent = temples[i].name; 
+
+            // Photo
+            let photo = temples[i].name.split(" ").join("");
+            console.log(photo);
+            fPhoto.setAttribute('src','images/' + photo + 'Hotel.jpg');
+            fPhoto.setAttribute('alt', fDesc.textContent + ' Hotel');
+
+            fCard.appendChild(fPhoto);
+            fCard.appendChild(fDesc);  
+
+            // A tag
+            fLink.appendChild(fCard);
+            fLink.setAttribute('href', photo + '.html');
+            
+            // Append to Doc
+            document.querySelector('div.locGallery').appendChild(fLink);
+        }
+
+    }
+});
 });
 
